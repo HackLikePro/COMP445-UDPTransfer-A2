f3a3f2028d1ed732827159288a3d616c09500056
HackLikePro <ctwdsale@gmail.com>
2016-03-21 23:35:06 -0400

    99% finsih
    99% finsih

--- src/UDPClient/UDPClient.java
+++ src/UDPClient/UDPClient.java
@@ -9,15 +9,20 @@ public class UDPClient {
 	static int packetSize = 1024;
 	static int port = 4214;
 	static int sequenceNumber = 0;
+	static int packetCount = 0;
+	static String fileName;
+	static String downorUp;
+	static String serverName;
+	static int packetdrop  = 4;
 
 	public static void main(String args[]) throws Exception {
 		InetAddress serverIP;
 		int serverPort;
 		creatConnection();
-		InetAddress clientIP = InetAddress.getByName("127.0.0.1");
+		InetAddress clientIP = InetAddress.getByName(serverName);
 		clientSocket = new DatagramSocket(port);
-		sendRequestPacket(clientIP, "thisname.jpg");
-		byte[][] byteFile = new byte[22][900];
+		sendRequestPacket(clientIP, fileName);
+		byte[][] byteFile = null ;
 
 		// client socket timeout -- end downloading or has issue with connection
 		clientSocket.setSoTimeout(3000);
@@ -25,7 +30,6 @@ public class UDPClient {
 		// get data reply test
 		while (true) {
 			try {
-				int packetcount = 0;
 				byte[] receivedData = new byte[packetSize];
 				DatagramPacket receivedPacket = new DatagramPacket(receivedData, packetSize);
 				clientSocket.receive(receivedPacket);
@@ -33,21 +37,29 @@ public class UDPClient {
 				serverIP = receivedPacket.getAddress();
 				serverPort = receivedPacket.getPort();
 
-				System.out.println(new String(receivedPacket.getData()));
-
+				System.out.println("Receiver: received packet " + sequenceNumber );
+				
+				packetdrop = (int) (Math.random()*10);
+                if (packetdrop == 4)
+                {
+                	packetDrop(receivedPacket);
+                	System.out.println("Warnning: packet Dropped");
+                }
 				// check packet sequence
 				// check if packet has error
 				if (checkSequence(receivedPacket) && checkSum(receivedPacket)) {
 					sendACK(serverIP, serverPort);
-					saveData(receivedPacket, byteFile, packetcount);
-					packetcount++;
-					sequenceNumber = (sequenceNumber+1)%2;
+                    System.out.println("Receiver: sent an ACK for packet "+ sequenceNumber);
+					saveData(receivedPacket, byteFile, packetCount);
+					packetCount++;
+					sequenceNumber = (sequenceNumber + 1) % 2;
 				} else {
 					sendNCK(serverIP, serverPort);
 				}
 
 			} catch (SocketTimeoutException e) {
-				System.out.println("Downloading ends");
+				System.out.println("Receiver: transfer completed");
+				System.out.println("Receiver: number of bytes received: " + packetCount);
 				System.exit(0);
 
 			}
@@ -55,53 +67,69 @@ public class UDPClient {
 
 	}
 
+
+	private static void packetDrop(DatagramPacket receivedPacket) {
+		// TODO Auto-generated method stub
+		byte [] dropbyte = new byte [1024];
+		Arrays.fill(dropbyte, (byte)'#');
+		receivedPacket.setData(dropbyte);
+	}
+
+
+	private static void saveData(DatagramPacket receivedPacket, byte[][] byteFile, int packetcount) throws IOException {
+		// TODO Auto-generated method stub
+
+		byte[] temp = receivedPacket.getData();
+		byte[] header = new byte[64];
+		for (int i = 0; i < 64; i++) {
+			header[i] = temp[i];
+		}
+		String[] headerSplit = new String(header).split(" ");
+		int fileSize = Integer.parseInt(headerSplit[8].trim());
+		byteFile = new byte[fileSize][1024];
+
+		byte[] temp2 = new byte[960];
+		for (int i = 0; i < 960; i++) {
+			temp2[i] = temp[i + 64];
+		}
+
+		byteFile[packetcount] = temp2;
+
+		saveFile(byteFile, packetcount);
+
+	}
+	
 	private static void saveFile(byte[][] byteFile, int packetcount) throws IOException {
 		// TODO Auto-generated method stub
 
-		String destination = new String("D:\\thisname.jpg");
+		String destination = new String("D:\\"+fileName);
 
 		FileOutputStream fos = new FileOutputStream(destination, true);
 		fos.write(byteFile[packetcount]);
 		fos.close();
 	}
 
-	private static void saveData(DatagramPacket receivedPacket, byte[][] byteFile, int packetcount) throws IOException {
-		// TODO Auto-generated method stub
-
-		byte [] temp = receivedPacket.getData();
-		byte [] temp2 = new byte [960];
-		for (int i = 0 ; i< 960 ;i++)
-		{
-			temp2[i] = temp[i+64];
-		}
-		
-		byteFile[packetcount] = temp2;
-
-		saveFile(byteFile, packetcount);
-
-	}
-
 	private static boolean checkSequence(DatagramPacket receivedPacket) {
 		// TODO Auto-generated method stub
-        byte [] temp = receivedPacket.getData();
-		byte [] header = new byte[64];
-        for (int i = 0; i<64 ; i++)
-        {
-		header[i] = temp[i];
-        }
-		String [] headerSplit = new String(header).split(" ");
-		String test1 = headerSplit[6];
-	    String test2 = Integer.toString(sequenceNumber);
-	    
-		if(test1.trim().equals(test2.trim()))
-		{
-			return true;
+		byte[] temp = receivedPacket.getData();
+		byte[] header = new byte[64];
+		for (int i = 0; i < 64; i++) {
+			header[i] = temp[i];
 		}
-		else
+		String[] headerSplit = new String(header).split(" ");
+		if(headerSplit.length<6)
 		{
 			return false;
 		}
-		
+		String test1 = headerSplit[6];
+		String test2 = Integer.toString(sequenceNumber);
+
+		if (test1.trim().equals(test2.trim())) {
+			return true;
+		} else {
+			return false;
+		}
+
 	}
 
 	private static boolean checkSum(DatagramPacket receivedPacket) {
@@ -115,7 +143,7 @@ public class UDPClient {
 		byte[] ACK = ACKrespon.getBytes();
 		DatagramPacket ACKpacket = new DatagramPacket(ACK, ACK.length, serverIP, serverPort);
 		clientSocket.send(ACKpacket);
-		System.out.println("ACK packet sent" + new String(ACKpacket.getData()));
+		
 	}
 
 	private static void sendACK(InetAddress serverIP, int serverPort) throws IOException {
@@ -124,12 +152,11 @@ public class UDPClient {
 		byte[] ACK = ACKrespon.getBytes();
 		DatagramPacket ACKpacket = new DatagramPacket(ACK, ACK.length, serverIP, serverPort);
 		clientSocket.send(ACKpacket);
-		System.out.println("ACK packet sent" + new String(ACKpacket.getData()));
-
+		
 	}
 
 	private static String sendRequestPacket(InetAddress serverIPAddress, String filename) throws Exception {
-		String request = ("GET " + filename + " HTTP/1.0\r\n");
+		String request = (downorUp.trim()+" " + filename + " HTTP/1.0\r\n");
 		byte[] requestData = new byte[request.length()];
 		requestData = request.getBytes();
 		DatagramPacket requestPacket = new DatagramPacket(requestData, requestData.length, serverIPAddress, 29723);
@@ -139,6 +166,18 @@ public class UDPClient {
 
 	private static void creatConnection() {
 		// TODO Auto-generated method stub
+		Scanner kb = new Scanner(System.in);
+		System.out.println("  *************************************************");
+		System.out.println("  *************************************************");
+		System.out.println("  **** Welcome to the UDP Stop and Wait System ****");
+		System.out.println("  *************************************************");
+		System.out.println("  *************************************************");
+		System.out.println("  Input the server name:                           ");
+		serverName = kb.nextLine();
+		System.out.println("  GET OR PUT ? :                                   ");
+		downorUp = kb.nextLine();
+		System.out.println("  What is the file's name ? :                      ");
+		fileName = kb.nextLine();
 
 	}
 }
--- src/UDPServer/UDPServer.java
+++ src/UDPServer/UDPServer.java
@@ -12,6 +12,13 @@ public class UDPServer {
 	final public static int packetSize = 1024;
 	final public static int headSize = 64;
 	final public static int timeout = 1000;
+	public static int filesize = 0;
+	public static int SequenceNum = 0;
+	public static int SequenceNumCount = 0;
+	public static int packetCount = 0;
+	public static int packetArrived = 0;
+	public static int effectivebytes = 0;
+	public static int totalbytes = 0;
 	static DatagramSocket serverSocket;
 
 	public static void main(String args[]) throws IOException {
@@ -36,7 +43,7 @@ public class UDPServer {
 			clientPort = receivedPacket.getPort();
 
 			// Debug:client source value
-			System.out.println("Recived request from Client IP:" + clientIP + " Port:" + clientPort);
+			System.out.println("Sender: starting on host " + InetAddress.getLocalHost().getHostName());
 
 			// Get client requested file name from in coming packet
 			requestedFileName = new String(receivedPacket.getData());
@@ -54,57 +61,65 @@ public class UDPServer {
 		// TODO Auto-generated method stub
 		boolean fileExsit = findFile(requestedFileName);
 		if (fileExsit) {
-			
+
 			// byte [][] fileData if it is split
 			byte[][] fileData = splitFile(requestedFileName);
 			DatagramPacket datapacket = null;
-			
-			// use a loop here to send all the file Data 
-			 for (int i = 0 ; i < fileData.length; i++)
-			 { 	
-				  datapacket = new DatagramPacket(fileData[i], fileData[i].length, clientIP, clientPort);
-                  System.out.println("packet created");			 
-			 
-  
-			//DatagramPacket datapacket = new DatagramPacket(fileData, fileData.length, clientIP, clientPort);
 
-			// set serverSocket time out
-			serverSocket.setSoTimeout(timeout);
-			// Note: set a while true here for resends missing packet
-			// set up ACK
-			byte[] ACK = new byte[packetSize];
-			DatagramPacket getAck = new DatagramPacket(ACK, ACK.length);
-			serverSocket.send(datapacket);
+			// use a loop here to send all the file Data
+			for (int i = 0; i < fileData.length; i++) {
+				datapacket = new DatagramPacket(fileData[i], fileData[i].length, clientIP, clientPort);
+				//System.out.println("packet created");
 
-			// Send packet and waiting for ACK reply
-			System.out.println("Waitting ACK respond starts");
-			while (true) {
-				try {
-					serverSocket.receive(getAck);
-					String ack = new String(getAck.getData());
-					System.out.println("\n-----FROM CLIENT-----\n" + ack.trim());
-					// Process ACK packet
+				// set serverSocket time out
+				serverSocket.setSoTimeout(timeout);
+				// Note: set a while true here for resends missing packet
+				// set up ACK
+				byte[] ACK = new byte[packetSize];
+				DatagramPacket getAck = new DatagramPacket(ACK, ACK.length);
+				serverSocket.send(datapacket);
+				packetCount++;
+				totalbytes = packetCount * 1024;
+				System.out.println("Sender: sent packet " +SequenceNumCount );
+				
+				// Send packet and waiting for ACK reply
+				//System.out.println("Waitting ACK respond starts");
+				while (true) {
+					try {
+						serverSocket.receive(getAck);
+						String ack = new String(getAck.getData());
+						// Process ACK packet
 
-					if (checkACK(ack.trim())) {
-						// Debug: ACK
-						System.out.println("got ACK");
-						break;
-					} else {
-						System.out.println("Wrong packet : resend");
+						if (checkACK(ack.trim())) {
+							// Debug: ACK
+							System.out.println("Sender: received ACK for packet "+SequenceNumCount );
+							SequenceNumCount = (SequenceNumCount + 1)%2;
+							packetArrived++;
+						    effectivebytes = packetArrived * 1024;
+							break;
+						} else {
+							System.out.println("Sender: Packet error - resend");
+							serverSocket.send(datapacket);
+							packetCount++;
+							continue;
+						}
+					} catch (SocketTimeoutException e) {
+						System.out.println("Sender: Time out - resend previous packet");
 						serverSocket.send(datapacket);
+						packetCount++;
 						continue;
+
 					}
-				} catch (SocketTimeoutException e) {
-					System.out.println("Time out : resend previous packet");
-					serverSocket.send(datapacket);
-					continue;
 
 				}
 
 			}
-			
-		}
-
+			serverSocket.setSoTimeout(0);
+			System.out.println("Sender: file transfer completed");
+			System.out.println("Sender: number of effective bytes sent: "+ effectivebytes);
+			System.out.println("Sender: number of packets sent: "+ packetCount);
+			System.out.println("Sender: number of bytes sent: "+ totalbytes);
+			System.exit(0);
 		}
 
 		else {
@@ -125,14 +140,14 @@ public class UDPServer {
 	private static byte[][] splitFile(String requestedFileName) throws IOException {
 		// TODO Auto-generated method stub
 		// Test: make all data one pack for testing wait and stop
-		int SequenceNum = 0; 
-		int size = 22;
+		File requestedFile = new File(requestedFileName);
+		filesize = (int) (requestedFile.length() / 960)+1;
 		java.nio.file.Path path = Paths.get(requestedFileName);
 		FileInputStream filestream = new FileInputStream(new File(requestedFileName));
 
-		byte[][] data =  new byte[size][1024];
+		byte[][] data = new byte[filesize][1024];
 		for (int i = 0; i < data.length; i++) {
-			addheader(data, i, SequenceNum);
+			addheader(data, i);
 			for (int j = 64; j < data[i].length; j++) {
 				data[i][j] = (byte) filestream.read();
 			}
@@ -142,22 +157,21 @@ public class UDPServer {
 		return data;
 	}
 
-	private static void addheader(byte[][] data, int i, int sequenceNum) {
+	private static void addheader(byte[][] data, int i) {
 		// TODO Auto-generated method stub
 		int checkSumRan = (int) Math.random();
-	    String checkSumResult = "Debug" ;
-		String header = " CheckSum: " + checkSumRan + " CheckSumResult: " + checkSumResult + " SequenceNum: " + sequenceNum;
-		byte [] temp = header.getBytes();
-		byte [] temp2 = new byte [64];
-        
-        for(int k = 0 ; k<header.getBytes().length; k++)
-        {
-        	temp2[k] = temp [k];
-        }
-		
-		for(int k = 0 ; k<64 ; k++)
-		{
-			data [i][k] = temp2[k];
+		String checkSumResult = "Debug";
+		String header = " CheckSum: " + checkSumRan + " CheckSumResult: " + checkSumResult + " SequenceNum: "
+				+ SequenceNum + " FileSize: " + filesize;
+		byte[] temp = header.getBytes();
+		byte[] temp2 = new byte[64];
+
+		for (int k = 0; k < header.getBytes().length; k++) {
+			temp2[k] = temp[k];
+		}
+
+		for (int k = 0; k < 64; k++) {
+			data[i][k] = temp2[k];
 		}
 
 	}
